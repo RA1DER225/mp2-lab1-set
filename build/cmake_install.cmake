b5995fc88869270dd4f7578cc1aa00d8f4c014fd
Новиков Даниил Алексеевич <s25380160@unn.ru>
2026-09-24 11:40:50 +0300

    GetBitClrBitSetBitGetLengthGetMemMaskGetMemIndex

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set
+# Install script for directory: C:/Users/s25380160/Desktop/git/mp2-lab1-set
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,17 +34,17 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/build/src/cmake_install.cmake")
-  include("C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/build/samples/cmake_install.cmake")
-  include("C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/build/gtest/cmake_install.cmake")
-  include("C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/build/test/cmake_install.cmake")
+  include("C:/Users/s25380160/Desktop/git/mp2-lab1-set/build/src/cmake_install.cmake")
+  include("C:/Users/s25380160/Desktop/git/mp2-lab1-set/build/samples/cmake_install.cmake")
+  include("C:/Users/s25380160/Desktop/git/mp2-lab1-set/build/gtest/cmake_install.cmake")
+  include("C:/Users/s25380160/Desktop/git/mp2-lab1-set/build/test/cmake_install.cmake")
 
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/s25380160/Desktop/git/mp2-lab1-set/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -60,6 +60,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/s25380160/Desktop/git/mp2-lab1-set/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- build/gtest/cmake_install.cmake
+++ build/gtest/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/gtest
+# Install script for directory: C:/Users/s25380160/Desktop/git/mp2-lab1-set/gtest
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,6 +35,6 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/helka/OneDrive/Desktop/git/Binary pole/mp2-lab1-set/build/gtest/install_local_manifest.txt"
+  file(WRITE "C:/Users/s25380160/Desktop/git/mp2-lab1-set/build/gtest/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
